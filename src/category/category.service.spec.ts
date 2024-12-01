422a1534793943df9cb30d382754c46f604609e1
batuhanisildak-malwation <batuhan.isildak@malwation.com>
2024-12-01 13:54:44 +0000

    fix(auth_test): fix the auth test

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Category } from '../entities/category.entity';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
+  const mockCategoryModel = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findById: jest.fn(),
+    create: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: getModelToken(Category.name),
+          useValue: mockCategoryModel,
+        },
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
