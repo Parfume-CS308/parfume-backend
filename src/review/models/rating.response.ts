f11afbf6f21dc1144cf12836b6ea82e6c90d7690
Eren Altın <erennaltin@gmail.com>
2024-12-03 22:39:09 +0300

    ratingCounts

--- src/review/models/rating.response.ts
+++ src/review/models/rating.response.ts
@@ -1,28 +1,41 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger'
 
 export class PerfumeRatingResponse {
   @ApiProperty({
     example: 4.5,
-    description: 'Average rating of the perfume',
+    description: 'Average rating of the perfume'
   })
-  averageRating: number;
+  averageRating: number
 
   @ApiProperty({
     example: 2,
-    description: 'Number of ratings given to the perfume',
+    description: 'Number of ratings given to the perfume'
   })
-  ratingCount: number;
+  ratingCount: number
 
   @ApiProperty({
     example: true,
-    description: 'Is the current user rated the perfume',
+    description: 'Is the current user rated the perfume'
   })
-  isRated: boolean;
+  isRated: boolean
 
   @ApiProperty({
     example: 4,
-    description:
-      'Rating of the current user, if rated it is number, otherwise null',
+    description: 'Rating of the current user, if rated it is number, otherwise null'
   })
-  userRating: number | null;
+  userRating: number | null
+
+  @ApiProperty({
+    example: {
+      1: 2,
+      2: 1,
+      3: 0,
+      4: 3,
+      5: 1
+    },
+    description: 'Rating counts of the perfume scaled from 1 to 5'
+  })
+  ratingCounts: {
+    [key: number]: number
+  }
 }
